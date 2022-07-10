e4a17c43a01e09f0c87f66318aeb8f02af282eba
Kunal Khatri <kunalkhatri017@gmail.com>
2022-07-10 11:30:12 +0530

    deployment on Heroku

--- my__project/settings.py
+++ my__project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#acrivate djangoheroku
+django_heroku.settings(locals())
